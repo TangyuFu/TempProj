354f693bd966147c6073d606ded8955ad86c5ccd
Tangyu Fu <futangyu@gmail.com>
2021-11-10 17:36:21 +0800

    添加场景 Identity 脚本
    用于摄像机场景挑选。

---
+++ Assets/GameFramework/Extension/Runtime/Base/Identity.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+namespace UnityGameFramework.Runtime.Extension
+{
+    /// <summary>
+    /// ID 依附于场景物体，用于射线检测。
+    /// </summary>
+    public class Identity : MonoBehaviour
+    {
+        [SerializeField] private int m_Id;
+
+        /// <summary>
+        /// Identity 的 Id.
+        /// </summary>
+        public int Id
+        {
+            get => m_Id;
+            set => m_Id = value;
+        }
+    }
+}
